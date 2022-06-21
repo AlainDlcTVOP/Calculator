4e69d49996eabdb9e1f209b46bc821ac3c930e95
AlainDlcTVOP <alain_dlc@outlook.com>
2022-06-21 10:01:35 +0200

    test

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+using System;
+
+namespace Asswigmen1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Push Test");
+        }
+    }
+}
